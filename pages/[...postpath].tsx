cd4154a832e9ebde1418f65cd385fa43132bf724
Sujon Kumar <38588193+sujonkumar@users.noreply.github.com>
2023-05-21 04:20:45 +0600

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					'https://powersportsliv.com' + encodeURI(path as string)
+					'https://powersportsliv.com/' + encodeURI(path as string)
 				}`,
 			},
 		};
